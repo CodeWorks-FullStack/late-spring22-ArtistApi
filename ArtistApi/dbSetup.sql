53c735f10347400f6707c05cb7d20748f7eaba97
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-07-20 12:32:21 -0600

    now with Populate and Auth!

--- ArtistApi/dbSetup.sql
+++ ArtistApi/dbSetup.sql
@@ -7,14 +7,20 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
 
-
+DROP TABLE artists;
 CREATE TABLE IF NOT EXISTS artists (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
   name VARCHAR(255) NOT NULL,
   dateOfBirth INT NOT NULL,
-  isAlive TINYINT NOT NULL
+  isAlive TINYINT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+
+  FOREIGN KEY (creatorId)
+    REFERENCES accounts(id)
+    ON DELETE CASCADE
+
 ) default charset utf8;
 
 
@@ -61,3 +67,11 @@ DROP TABLE artists;
 
 /* Remove entire DB */
 DROP DATABASE;
+
+
+
+      SELECT
+        a.*,
+        acts.*
+      FROM artists a
+      JOIN accounts acts ON acts.id = a.creatorId;
